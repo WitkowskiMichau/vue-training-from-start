a87c91e2d43bed5c12711ac6a617e3c34b32c9e3
Michal Witkowski <michal.witkowski@thegoodcode.io>
2024-05-28 21:00:12 +0200

    1 List of the cities

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
-<script setup lang="ts">
-</script>
-
 <template>
-Hello World
+  <div>
+    <label for="citySelect">Select a City:</label>
+    <select id="citySelect">
+      <option v-for="city in cityList" :key="city">{{ city }}</option>
+    </select>
+  </div>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+const cityList = [
+  'Holmdel, New Jersey',
+  'Atlanta, Georgia',
+  'San Francisco, California',
+  'Basingstoke, United Kingdom',
+  'London, Shoreditch, United Kingadom',
+  'London, Canary Wharf, United Kingdom',
+  'Tel Aviv-Yafo, Israel'
+];
+</script>
 
+<style scoped>
+#citySelect {
+  margin: 10px;
+  padding: 5px;
+  font-size: 16px;
+}
 </style>
