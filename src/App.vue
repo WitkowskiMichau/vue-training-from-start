ae276a5b9c84b0651c507ba007af00825a890f82
Michal Witkowski <michal.witkowski@thegoodcode.io>
2024-05-28 21:04:47 +0200

    vmodel and vif

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div>
     <label for="citySelect">Select a City:</label>
-    <select id="citySelect">
-      <option v-for="city in cityList" :key="city">{{ city }}</option>
+    <select id="citySelect" v-model="selectedCity">
+      <option value="" disabled>Select a city</option>
+      <option v-for="city in cityList" :key="city" :value="city">{{ city }}</option>
     </select>
+    <p v-if="selectedCity">Selected City: {{ selectedCity }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 const cityList = [
   'Holmdel, New Jersey',
   'Atlanta, Georgia',
   'San Francisco, California',
   'Basingstoke, United Kingdom',
-  'London, Shoreditch, United Kingadom',
+  'London, Shoreditch, United Kingdom',
   'London, Canary Wharf, United Kingdom',
   'Tel Aviv-Yafo, Israel'
 ];
+
+const selectedCity = ref('');
 </script>
 
 <style scoped>
